a7fc27442054ec2ef14502f5115ec3834e1ecefc
Krehion <krehion@hotmail.com>
2025-03-07 17:16:33 +0100

    :sparkles: add hero section

---
+++ ArgentBank-front/src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import "../../style/components/_hero.scss";
+
+export default function Hero() {
+	return (
+		<div className="hero">
+			<div className="hero--content">
+				<h2>No fees.</h2>
+				<h2>No minimum deposit.</h2>
+				<h2>High interest rates.</h2>
+				<p>Open a savings account with Argent Bank today!</p>
+			</div>
+		</div>
+	);
+}
--- ArgentBank-front/src/pages/index/Index.jsx
+++ ArgentBank-front/src/pages/index/Index.jsx
@@ -1,10 +1,17 @@
 import Nav from "../../components/nav/Nav";
+import Hero from "../../components/hero/Hero";
 
 const Index = () => {
 	return (
 		<>
 			<Nav />
-			<div>Prout</div>
+			<main>
+				<Hero />
+				<section className="features">
+					{/* Feature items: map an array of 3 elements with image + title + text */}
+				</section>
+			</main>
+			{/* Footer */}
 		</>
 	);
 };
