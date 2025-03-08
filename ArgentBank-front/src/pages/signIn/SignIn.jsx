653f825f4ad47fc323a1d2a64702c1bb25b261e2
Krehion <krehion@hotmail.com>
2025-03-08 16:00:58 +0100

    :sparkles: add signin form

--- ArgentBank-front/src/pages/signIn/SignIn.jsx
+++ ArgentBank-front/src/pages/signIn/SignIn.jsx
@@ -1,13 +1,36 @@
 import Nav from "../../components/nav/Nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import Footer from "../../components/footer/Footer";
+import "../../style/components/_signin.scss";
 
 const SignIn = () => {
 	return (
 		<>
 			<Nav />
-			<div>
-				<p>Prout</p>
-			</div>
+			<main className="bg-dark">
+				<div className="signin">
+					<FontAwesomeIcon icon={faCircleUser} />
+					<h1>Sign in</h1>
+					<form className="signin--form">
+						<div className="signin--form--input-wrapper">
+							<label for="username">Username</label>
+							<input id="username" type="text" />
+						</div>
+						<div className="signin--form--input-wrapper">
+							<label for="password">Password</label>
+							<input id="password" type="password" />
+						</div>
+						<div className="signin--form--input-remember">
+							<input id="rememberme" type="checkbox" />
+							<label for="rememberme">Remember me</label>
+						</div>
+						<button type="submit" className="signin--form--btn">
+							Sign in
+						</button>
+					</form>
+				</div>
+			</main>
 			<Footer />
 		</>
 	);
