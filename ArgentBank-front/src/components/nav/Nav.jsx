972bc1f985b7d4362da94f9a21baeba1dbe9c1a5
Krehion <krehion@hotmail.com>
2025-03-09 15:35:02 +0100

    :sparkles: add logout & user to navbar

--- ArgentBank-front/src/components/nav/Nav.jsx
+++ ArgentBank-front/src/components/nav/Nav.jsx
@@ -1,19 +1,41 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/authSlice";
 import logo from "../../assets/argentBankLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import "../../style/components/_nav.scss";
 
 export default function Nav() {
+	const { user, isAuthenticated } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		dispatch(logout());
+		navigate("/");
+	};
+
 	return (
 		<nav className="main-nav">
 			<NavLink to={"/"} className={"main-nav--logo"}>
 				<img src={logo} />
 			</NavLink>
 			<div className="main-nav--items-container">
-				<NavLink to={"sign-in"} className="main-nav--item">
-					<FontAwesomeIcon icon={faCircleUser} /> Sign in
-				</NavLink>
+				{isAuthenticated ? (
+					<>
+						<NavLink to={"/user"} className="main-nav--item">
+							<FontAwesomeIcon icon={faCircleUser} /> {user?.firstName}
+						</NavLink>
+						<a onClick={handleLogout} className="main-nav--item">
+							<FontAwesomeIcon icon={faRightFromBracket} /> Log out
+						</a>
+					</>
+				) : (
+					<NavLink to={"sign-in"} className="main-nav--item">
+						<FontAwesomeIcon icon={faCircleUser} /> Sign in
+					</NavLink>
+				)}
 			</div>
 		</nav>
 	);
